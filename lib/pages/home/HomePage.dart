57f2409617216481ead59679df4edf416ea5f611
Osemwingie Oshodin <osemwingieoshodin@gmail.com>
2020-05-30 15:23:07 +0100

    Adjusted the UI

--- lib/pages/home/HomePage.dart
+++ lib/pages/home/HomePage.dart
@@ -9,13 +9,31 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Stack(fit: StackFit.expand, children: <Widget>[
-      CircleAvatar(
-        child: Image.asset("assests/bg.jpg"),
-      ),
-      Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[Text(""), Text("")])
-    ]));
+        backgroundColor: Colors.grey.shade900,
+        body: Container(
+          padding: EdgeInsets.all(20.0),
+          child: Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  CircleAvatar(
+                    radius: 60,
+                    backgroundImage: AssetImage("assests/img.jpg"),
+                  ),
+                  Padding(padding: EdgeInsets.only(top: 40)),
+                  Text("My name is Ose",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold)),
+                  Padding(padding: EdgeInsets.only(top: 20)),
+                  Text("Email is 0se@...",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold)),
+                ]),
+          ),
+        ));
   }
 }
